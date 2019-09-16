40adba2ca1aac02e8157dd56cafe059ad4a43957
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-16 02:25:37 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     session[:name] = params[:name]
     redirect_to '/'
     if session[:name] == nil
-      redirect_to '/login'
+      redirect_to '/'
     else
     end
   end
