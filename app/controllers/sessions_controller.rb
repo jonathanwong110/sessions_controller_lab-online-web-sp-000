0605c90e5b89a61de52f043392d3819980b8445f
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-16 02:34:34 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
   
   def create
     if params[:name] == nil
-      redirect_to '/new'
+      redirect_to '/sessions/new'
     elsif params[:name] == ""
-      redirect_to '/new'
+      redirect_to '/sessions/new'
     else
       session[:name] = params[:name]
       redirect_to '/'
